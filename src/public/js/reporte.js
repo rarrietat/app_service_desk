e647e8aea606443aff27ba6dc83bed30366c9525
rarrietat <rarrietat@gmail.com>
2021-06-01 14:10:09 -0500

    insert charts on reports

--- src/public/js/reporte.js
+++ src/public/js/reporte.js
@@ -7,12 +7,14 @@ const porcentajeSiHito3 = document.querySelector('#porcentajeSiHito3')
 const cumplimientoNoHito3 = document.querySelector('#cumplimientoNoHito3')
 const porcentajeNoHito3 = document.querySelector('#porcentajeNoHito3')
 const totalHito3 = document.querySelector('#totalHito3')
+const totalPorcentajeHito3 = document.querySelector('#totalPorcentajeHito3')
 
 const cumplimientoSiHito4 = document.querySelector('#cumplimientoSiHito4')
 const porcentajeSiHito4 = document.querySelector('#porcentajeSiHito4')
 const cumplimientoNoHito4 = document.querySelector('#cumplimientoNoHito4')
 const porcentajeNoHito4 = document.querySelector('#porcentajeNoHito4')
 const totalHito4 = document.querySelector('#totalHito4')
+const totalPorcentajeHito4 = document.querySelector('#totalPorcentajeHito4')
 
 async function listYear(){
 
@@ -100,6 +102,8 @@ selectYear.addEventListener('change', async () => {
     await listMonth(year)
     const month = await selectMonth.value
     await listReport(year, month)
+    await renderCharts(year, month)
+    await validar()
     
 })
 
@@ -108,8 +112,163 @@ selectMonth.addEventListener('change', async () => {
     const year = await selectYear.value
     const month = await selectMonth.value
     await listReport(year, month)
+    await renderCharts(year, month)
+    await validar()
     
 })
+
+let chartsHito3
+let chartsHito4
+
+//## reports
+async function totalCasesChart(ctx1, ctx2, year, month){
+
+    if(chartsHito3) chartsHito3.destroy()
+    if(chartsHito4) chartsHito4.destroy()
+
+    const dataHito3 = []
+    const dataHito4 = []
+
+    await fetch(`/api/reporte/${year}/${month}`)
+        .then(async res => await res.json())
+        .then(async data => {
+            data.hito3.forEach(e => {
+                if(e._id == 'si'){
+                    dataHito3.push(e.cumplimientoSi)
+                }else{
+                    dataHito3.push(e.cumplimientoNo)
+                }
+            })
+
+            data.hito4.forEach(e => {
+                if(e._id == 'si'){
+                    dataHito4.push(e.cumplimientoSi)
+                }else{
+                    dataHito4.push(e.cumplimientoNo)
+                }
+            })
+        })
+
+    if (chartsHito3) {
+        chartsHito3.destroy()
+    }
+    if (chartsHito4) {
+        chartsHito4.destroy()
+    }
+
+    chartsHito3 = new Chart(ctx1, {
+        type: 'bar',
+        data: {
+            labels: ['Cumplimiento', 'No Cumplimiento'],
+            datasets: [{
+                label: 'Averias',
+                data: dataHito3,
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(255, 99, 132, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(255, 99, 132, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                  display: false
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    })
+
+    chartsHito4 = new Chart(ctx2, {
+        type: 'bar',
+        data: {
+            labels: ['Cumplimiento', 'No Cumplimiento'],
+            datasets: [{
+                label: 'Averias',
+                data: dataHito4,
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(255, 99, 132, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(255, 99, 132, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                  display: false
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    })
+
+}
+
+async function renderCharts(year, month){
+    const ctx1 = document.querySelector('#chartHito3').getContext('2d')
+    const ctx2 = document.querySelector('#chartHito4').getContext('2d')
+    if(ctx1.chartHito3){
+        chartHito3.destroy();
+    }
+
+    if(ctx2.chartHito4){
+        chartHito4.destroy();
+    }
+
+    await totalCasesChart(ctx1, ctx2, year, month)
+}
+
+async function validar(){
+    if(cumplimientoSiHito3.innerHTML == ''){
+        cumplimientoSiHito3.innerHTML = 0
+        porcentajeSiHito3.innerHTML = '0 %'
+    }
+
+    if(cumplimientoNoHito3.innerHTML == ''){
+        cumplimientoNoHito3.innerHTML = 0
+        porcentajeNoHito3.innerHTML = '0 %'
+    }
+
+    if(totalHito3.innerHTML == ''){
+        totalHito3.innerHTML = 0
+        totalPorcentajeHito3.innerHTML = '0 %'
+    }else totalPorcentajeHito3.innerHTML = '100 %'
+
+    
+    if(cumplimientoSiHito4.innerHTML == ''){
+        cumplimientoSiHito4.innerHTML = 0
+        porcentajeSiHito4.innerHTML = '0 %'
+    }
+
+    if(cumplimientoNoHito4.innerHTML == ''){
+        cumplimientoNoHito4.innerHTML = 0
+        porcentajeNoHito4.innerHTML = '0 %'
+    }
+
+    if(totalHito4.innerHTML == ''){
+        totalHito4.innerHTML = 0
+        totalPorcentajeHito4.innerHTML = '0 %'
+    }else totalPorcentajeHito4.innerHTML = '100 %'
+
+}
 
 window.addEventListener('load', async () => {
 
@@ -118,5 +277,7 @@ window.addEventListener('load', async () => {
     await listMonth(year)
     const month = await selectMonth.value
     await listReport(year, month)
+    await renderCharts(year, month)
+    await validar()
 
 })
